85aaa5e2993a8aaf6309c68df71971ea3ad4a332
Razvan Popa <razvan@itnovate.ro>
2021-03-22 11:48:24 -0700

    Adds element prepending (#1)

--- src/models/web_content.model.ts
+++ src/models/web_content.model.ts
@@ -2,6 +2,8 @@ export interface WebItem {
 	url: string
 	zoom?: number
 	scroll_to?: number
+	prepend_element_by_class_to_body?: string
+	prepend_element_by_id_to_body?: string
 }
 
 export interface Group {
